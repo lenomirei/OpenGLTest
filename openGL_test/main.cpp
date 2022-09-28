497de3a4a09279ea9ee387d233c79ec261466b59
lenomirei <lenomirei@163.com>
2022-09-28 18:13:51 +0800

    texture test

--- openGL_test/main.cpp
+++ openGL_test/main.cpp
@@ -4,27 +4,31 @@
 #include <iostream>
 #include <cmath>
 
-
+#include "image/FreeImage.h"
 
 #define GET_STR(x) #x
 const char* vertexShaderSource = GET_STR(
     #version 330 core\n
     layout(location = 0) in vec3 position;
-    
-    void main()
-    {
-        gl_Position = vec4(position.x, position.y, position.z, 1.0);
-    }
+layout(location = 1) in vec2 texCoord;
+
+out vec2 TexCoord;
+
+void main()
+{
+    gl_Position = vec4(position, 1.0f);
+    TexCoord = texCoord;
+}
 );
 
 const char* fragmentShaderSource = GET_STR(
     #version 330 core\n
-    out vec4 color;
-    uniform vec4 ourColor;
-    void main()
-    {
-        color = ourColor;
-    }
+    in vec2 TexCoord;
+uniform sampler2D ourTexture;
+void main()
+{
+    color = texture(ourTexture, TexCoord);
+}
 );
 
 int CreateShader()
@@ -86,20 +90,18 @@ int main()
         return -1;
     }
 
-    int width, height;
-    glfwGetFramebufferSize(window, &width, &height);
 
-    glViewport(0, 0, width, height);
+    glViewport(0, 0, 800, 600);
 
     int shaderProgram = CreateShader();
 
     // Vertex 
     // x y z
     GLfloat vertices[] = {
-        0.5f, 0.5f, 0.0f,
-        0.5f, -0.5f, 0.0f,
-        -0.5f, -0.5f, 0.0f,
-        -0.5f, 0.5f, 0.0f
+        0.5f, 0.5f, 0.0f, 1.0f, 1.0f,
+        0.5f, -0.5f, 0.0f, 1.0f, -1.0f,
+        -0.5f, -0.5f, 0.0f, -1.0f, -1.0f,
+        -0.5f, 0.5f, 0.0f, -1.0f, 1.0f
     };
 
     GLuint indices[] = {
@@ -116,43 +118,66 @@ int main()
 
     GLuint EBO;
     glGenBuffers(1, &EBO);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
     // ..:: 初始化代码（只运行一次 (除非你的物体频繁改变)） :: ..
 // 1. 绑定VAO
     glBindVertexArray(VAO);
-        // 2. 把顶点数组复制到缓冲中供OpenGL使用
-        glBindBuffer(GL_ARRAY_BUFFER, VBO);
-        glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-
-        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-        glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
-        // 3. 设置顶点属性指针
-        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), (GLvoid*)0);
-        glEnableVertexAttribArray(0);
+    // 2. 把顶点数组复制到缓冲中供OpenGL使用
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+    // 3. 设置顶点属性指针
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(GLfloat), (GLvoid*)0);
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(GLfloat), (GLvoid*)(0)(3 * sizeof(GLfloat)));
+    glEnableVertexAttribArray(0);
     //4. 解绑VAO
     glBindVertexArray(0);
+
+    // texture
+    GLuint texture;
+    glGenTextures(1, &texture);
+
+    glBindTexture(GL_TEXTURE_2D, texture);
+    // Set our texture parameters
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);	// Set texture wrapping to GL_REPEAT
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+    // Set texture filtering
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+    // load image 
+    int width, height;
+
+    FIBITMAP* dib = FreeImage_Load(FIF_JPEG, "container.jpg");
+    width = FreeImage_GetWidth(dib);
+    height = FreeImage_GetHeight(dib);
+
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, dib->data);
+    glGenerateMipmap(GL_TEXTURE_2D);
+    glBindTexture(GL_TEXTURE_2D, 0);
+
     // event loop
-    glBindVertexArray(VAO);
     glUseProgram(shaderProgram);
     while (!glfwWindowShouldClose(window))
     {
         glfwPollEvents();
 
-
-        GLfloat timeValue = glfwGetTime();
-        GLfloat greenValue = (sin(timeValue) / 2) + 0.5;
-        GLint vertexColorLocation = glGetUniformLocation(shaderProgram, "ourColor");
-        
-        glUniform4f(vertexColorLocation, 0.0f, greenValue, 0.0f, 1.0f);
         // draw zone
-        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+        glActiveTexture(GL_TEXTURE0);
+        glBindTexture(GL_TEXTURE_2D, texture);
+        glUniform1i(glGetUniformLocation(shaderProgram, "ourTexture"), 0);
 
+        glBindVertexArray(VAO);
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+        glBindVertexArray(0);
 
         // double buffer
         glfwSwapBuffers(window);
     }
-    glBindVertexArray(0);
-
+    glDeleteVertexArrays(1, &VAO);
+    glDeleteBuffers(1, &VBO);
+    glDeleteBuffers(1, &EBO);
 
     glfwTerminate();
     return 0;
